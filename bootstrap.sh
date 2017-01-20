64750ad23f162a73f42422fd72e2d7e16d3ec107
Luke Karrys <lukekarrys@gmail.com>
2017-01-20 15:03:00 -0700

    fix npm ls global listing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env zsh
 
 DUMP=$1
-MACOS_VERSION=$(sw_vers -productVersion)
+
+function npm-ls-global() {
+  local MODULES=$(npm ls -g --depth=0 --parseable | tail -n +2)
+  while IFS= read -r MODULE; do
+    local DIR=$(dirname $MODULE | xargs -n1 basename)
+    local BASE=$(basename $MODULE)
+    local INSTALLED_MODULE=''
+    if [ "$DIR" != "node_modules" ]; then
+      INSTALLED_MODULE="$DIR/$BASE"
+    else
+      INSTALLED_MODULE="$BASE"
+    fi
+    echo $INSTALLED_MODULE;
+  done <<< "$MODULES"
+}
 
 if [[ "$DUMP" == "--dump" ]] then
   echo "Dumping npm and brew"
-  npm ls -g --depth=0 --parseable | tail -n +2 | xargs -n1 basename > installed-packages/npm.txt
+  npm-ls-global > installed-packages/npm.txt
   brew bundle dump --file=installed-packages/Brewfile --force
 fi
 
@@ -18,12 +32,6 @@ rsync --exclude ".git/" \
       --exclude "oh-my-zsh/" \
       --exclude "installed-packages/" \
       -avh --no-perms . ~;
-
-# Super hack. UseKeychain only works in 10.12.x
-# TODO: Needs to be updated for other versions obviously.
-if [[ "$MACOS_VERSION" == "10.12."* ]] then
-  sed -i -- 's/# UseKeychain/UseKeychain/g' ~/.ssh/config
-fi
 
 source ~/.zprofile;
 source ~/.zshrc;
