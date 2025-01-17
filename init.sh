766ff1f08a4c7cdbed0e4dae7b35923c934e0a0b
Luke Karrys <luke@lukekarrys.com>
2025-01-17 15:15:13 -0700

    add back repo to init

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /tmp/bin init --ssh --apply --force --source "$HOME/.local/data/chezmoi"
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /tmp/bin --ssh --apply --force --source "$HOME/.local/data/chezmoi" lukekarrys/dotfiles
