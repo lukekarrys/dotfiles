08b27643c75de7f4a6648d69ab9e1660f226fb6f
Luke Karrys <luke@lukekarrys.com>
2025-01-17 09:14:11 -0700

    homebrew has to be in init

--- pre.sh
+++ pre.sh
@@ -2,19 +2,19 @@
 
 case "$(uname -s)" in
 Darwin)
-    # Install 1Password if it's not installed
-    if ! type op &> /dev/null; then
-        echo "Installing 1Password..."
-        brew install --cask --adopt 1password
-        brew install --cask --adopt 1password-cli
-        echo "Opening 1Password. Please log into your account and enable CLI integration."
-        open "/Applications/1Password.app"
-        echo "Press Enter to continue."
-        read
-    fi
-    ;;
+  # Install 1Password if it's not installed
+  if ! type op &> /dev/null; then
+    echo "Installing 1Password..."
+    brew install --cask --adopt 1password
+    brew install --cask --adopt 1password-cli
+    echo "Opening 1Password. Please log into your account and enable CLI integration."
+    open "/Applications/1Password.app"
+    echo "Press Enter to continue."
+    read
+  fi
+  ;;
 *)
-    echo "unsupported OS"
-    exit 1
-    ;;
+  echo "unsupported OS"
+  exit 1
+  ;;
 esac
